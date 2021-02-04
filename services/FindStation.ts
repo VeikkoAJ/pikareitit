07ab70c704c4d5aa9793cac715444987e671b3af
VeikkoJ <veikko.jaaskelainen@aalto.fi>
2021-02-04 20:15:37 +0200

    fixed station search to ignore case

--- services/FindStation.ts
+++ services/FindStation.ts
@@ -1,12 +1,17 @@
 import { Station, Stop } from '../transitStopsQueryTypes';
 
 /**
- * Filters the stations by given search word
+ * Filters the stations by given searchWord word. return 4 nearest stops
  * @param stations list of all stations
- * @param search search word
+ * @param searchWord
  */
-export const FindStation = (stations: Station[], search: string): Station[] =>
-  stations.filter((station) => station.name.includes(search)).slice(0, 3);
+export const FindStation = (
+  stations: Station[],
+  searchWord: string
+): Station[] =>
+  stations
+    .filter((station) => station.name.toLowerCase().includes(searchWord))
+    .slice(0, 5);
 
 const distance = (stop: Stop, lat: number, lon: number) => {
   const latSquare = (stop.lat - lat) * (stop.lat - lat);
@@ -14,16 +19,20 @@ const distance = (stop: Stop, lat: number, lon: number) => {
   return Math.sqrt(latSquare + lonSquare);
 };
 
+/**
+ * Sorts the stops by closest to th given point. returns 4 nearest stops
+ * @param stops list of all stops
+ * @param lat latitude of the point
+ * @param lon longitude of the point
+ */
 export const FindNearestStops = (
   stops: Stop[],
   lat: number,
   lon: number
-): Stop[] => {
-  const filteredStops = [...stops]
+): Stop[] =>
+  [...stops]
     .sort(
       (stopA: Stop, stopB: Stop) =>
         distance(stopA, lat, lon) - distance(stopB, lat, lon)
     )
     .slice(0, 5);
-  return filteredStops;
-};
